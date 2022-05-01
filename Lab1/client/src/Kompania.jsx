f2f0514b41cdeaa64d5b6a8410975a5c85996274
Bleona Gerbavci <bg52732@ubt-uni.net>
2022-05-01 05:52:30 -0400

    CRD

--- Lab1/client/src/Kompania.jsx
+++ Lab1/client/src/Kompania.jsx
@@ -1,5 +1,6 @@
 import React,{Component, useState, useEffect } from 'react';
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
 
 
 export default function Kompania() {
@@ -7,7 +8,8 @@ export default function Kompania() {
     const[kompanite, setKompanite] = useState([]);
 
     const [refreshKey, setRefreshKey] = useState('0');
-
+    
+    //get data from database
     useEffect(() => {
         axios.get('https://localhost:7138/api/Kompania/GetKompanite')
             .then(response => {
@@ -15,8 +17,94 @@ export default function Kompania() {
             })
     }, [refreshKey])
 
+
+
+
+    //Delete data in database    
+    function deleteKompania(kompaniaId) {
+        const confirmBox = window.confirm(
+            "A jeni i sigurte qe po doni me fshi kompanine me id " + kompaniaId  +"? Mos paqsh mo pune me ta. " 
+        )
+        if (confirmBox === true) {
+            axios.delete('https://localhost:7138/api/Kompania/FshijKompanine?id=' + kompaniaId)
+                .then(() => {
+                    setRefreshKey(refreshKey => refreshKey + 1)
+                })
+        }
+        else {
+            console.log("Process of deleting a company canceled !!");
+        }
+    }
+
+    //Set data to database
+    const [name, setName] = useState('');
+    const [adress, setAdress] = useState('');
+    const [city, setCity] = useState('');
+    const [email, setEmail] = useState('');
+    const [contactNumber, setContactNumber] = useState('');
+    
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+        const kompania = { name, adress, city, email, contactNumber };
+
+        axios.post('https://localhost:7138/api/Kompania/ShtoKompani', kompania)
+            .then(() => {
+               window.confirm('Kompania u shtua me sukses! Perhajr...')
+            })
+            .then(() => {
+            setRefreshKey(refreshKey => refreshKey + 1)
+        })
+    }
+
         return(
             <div>
+
+            <form onSubmit={handleAdd}>
+                                <TextField
+                                    required
+                                    id="filled-required"
+                                    label="Name"
+                                    variant="standard"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            <TextField
+                                id="filled-number"
+                                label="Adress"
+                                variant="standard"
+                                value={adress}
+                                onChange={(e) => setAdress(e.target.value)}
+                            />
+                            <TextField
+                                id="filled"
+                                label="City"
+                                variant="standard"
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                            />
+                            <TextField
+                                id="filled"
+                                label="Email"
+                                variant="standard"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                             <TextField
+                                id="filled"
+                                label="Numri Kontaktues"
+                                type="number"
+                                variant="standard"
+                                value={contactNumber}
+                                onChange={(e) => setContactNumber(e.target.value)}
+                            />
+                           
+                           <button type="submit" className="btn btn-outline-secondary">
+                               Shto Kompanine
+                            </button>
+                </form>
+                <br />
+                <br />
                 <table className="table table-striped">
                     <thead>
                         <tr>
@@ -39,14 +127,14 @@ export default function Kompania() {
                                <td>{kompania.email}</td>
                                <td>{kompania.contactNumber}</td>
                                <td>
-                                  <button type="button"
+                                  <button type="button"  
                                    className="btn btn-light mr-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                     </svg>
                                    </button>
-                                   <button type="button"
+                                   <button type="button" onClick={() => deleteKompania(kompania.id)}
                                    className="btn btn-light mr-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
